4785a2ea8c850e548c7ff5993b90b665c7395937
BreadWave <vildosolaluciano@gmail.com>
2016-11-29 22:50:19 -0300

    Chapter 9 Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :microposts
-  resources :users
+	root 'static_pages#home'
 
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
+	get  '/help',    to: 'static_pages#help'
+	get  '/about',   to: 'static_pages#about'
+	get  '/contact', to: 'static_pages#contact'
+	get  '/signup',  to: 'users#new'
+	post '/signup',  to: 'users#create'
 
+	get    '/login',  to: 'sessions#new'
+	post   '/login',  to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
 
-  root 'static_pages#home'
+	resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
